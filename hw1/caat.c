00f7518968f2005cd6f0d46486d5db84cc1e76e8
alvdao <alvdao@ucsc.edu>
2025-01-17 05:16:47 +0000

    initial version, wip

---
+++ hw1/caat.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(void) {
+  int ch;
+  while ((ch = getchar()) != EOF) { // this one's gonna be a long one. gonna brute force this one
+    if (ch == 'a') { // start with lowercase vowels
+      putchar('%c', ch);
+    }
+    else if (ch == 'e')
+    {
+      putchar('%c', ch);
+    }
+    else if (ch == 'i')
+    {
+      putchar('%c', ch);
+    }
+    else if (ch == 'o')
+    {
+      putchar('%c', ch);
+    }
+    else if (ch == 'u')
+    {
+      putchar('%c', ch);
+    }
+    else if (ch == 'A') // then uppercase vowels
+    {
+      putchar('%c', ch);
+    }
+    else if (ch == 'E')
+    {
+      putchar('%c', ch);
+    }
+    else if (ch == 'I')
+    {
+      putchar('%c', ch);
+    }
+    else if (ch == 'O')
+    {
+      putchar('%c', ch);
+    }
+    else if (ch == 'U')
+    {
+      putchar('%c', ch);
+    }
+    else if
+    {
+      putchar('%c', ch);
+    }
+    else
+    {
+      putchar('', ch);
+    }
+}
