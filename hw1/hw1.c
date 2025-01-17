4cb97ce74b71155def29ec5025e56c5df07750c3
alvdao <alvdao@ucsc.edu>
2025-01-17 04:27:34 +0000

    hw1.c initial ver for review

---
+++ hw1/hw1.c
@@ -0,0 +1,43 @@
+#include "hw1.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+// For the value of Pi, please use M_PI, which is defined in math.h.
+double area_of_disk(double radius) {
+#define M_PI 3.14159265358979323846 // Defined Pi
+    double area = M_PI * (radius * radius); // A = pi * r^2
+  return area; // Store area value
+}
+
+double area_of_ring(double outer_radius, double inner_radius) {
+  double outer_area = area_of_disk(outer_radius);
+  double inner_area = area_of_disk(inner_radius);
+  return outer_area - inner_area; // Idea is to calculate the outer and inner ring area separately, then subtract the outer value from the inner to get the ring area.
+}
+
+int bigger_minus_smaller(int a, int b) {
+  if (a > b) {
+    return a - b;
+  } else {
+    return b - a;
+  }
+}
+
+bool value_in_range(int lower_bound, int x, int upper_bound) {
+  if (x >= lower_bound && x <= upper_bound) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+int sum_of_greater_squares(int a, int b, int c) {
+  if (a <= b && a <= c) {
+    return (b * b) + (c * c);
+  } else if (b <= a && b <= c) {
+    return (a * a) + (c * c);
+  } else { 
+    return (a * a) + (b * b);
+  }
+}
