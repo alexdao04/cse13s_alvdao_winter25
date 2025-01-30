c727a317fc540e86745cfd64038b9016531fd4e8
alexdao <alvdao@ucsc.edu>
2025-01-30 06:56:48 +0000

    commenting stuff out

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -10,54 +10,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool str_suffix(char *haystack, char *needle) {
+bool str_suffix(char *haystack, char *needle) { // returns true if the string "needle" is a suffix of the string "haystack"
   UNUSED(haystack);
   UNUSED(needle);
   return false;
 }
 
-char *str_repeat(char *s, int n) {
+char *str_repeat(char *s, int n) { // returns a string thats the input string "s" repeated "n" times
   UNUSED(s);
   UNUSED(n);
 
-  char *out = malloc(sizeof(char) * 1);
+  char *out = malloc(sizeof(char) * 1); // allocates memory for the output string
   out[0] = '\0';
 
-  return out;
+  return out; // returns the output string
 }
 
-bool str_is_palindrome(char *s) {
+bool str_is_palindrome(char *s) { // returns true if the string "s" is a palindrome
   UNUSED(s);
   return false;
 }
 
-float dot_product(float *vec_a, float *vec_b, int length) {
+float dot_product(float *vec_a, float *vec_b, int length) { // returns the dot product of two vectors
   UNUSED(vec_a);
   UNUSED(vec_b);
   UNUSED(length);
-  return 0;
+  return 0; // sum of the products of the respective elements of the two arrays
 }
 
-int reduce(int *nums, int length, int (*f)(int, int), int initial) {
+int reduce(int *nums, int length, int (*f)(int, int), int initial) { // combines all elements of an array into a single value
   UNUSED(nums);
   UNUSED(length);
   UNUSED(f);
   UNUSED(initial);
-  return 0;
+  return 0; // returns the final value
 }
 
 // These two functions will end up as a *single call to reduce*, but you'll have
 // to write a new helper function for each of them. Those helper functions will
 // be quite short though!
 
-int maximum_with_reduce(int *nums, int length) {
+int maximum_with_reduce(int *nums, int length) { // does the same thing as maximum, but calls reduce
   UNUSED(nums);
   UNUSED(length);
-  return 0;
+  return 0; // returns the maximum element in the array
 }
 
-int sum_positive_with_reduce(int *nums, int length) {
+int sum_positive_with_reduce(int *nums, int length) { // does the same thing as sum_positive, but calls reduce
   UNUSED(nums);
   UNUSED(length);
-  return 0;
+  return 0; // returns the sum of all positive elements in the array
 }
--- hw3/hw3.h
+++ hw3/hw3.h
@@ -1,19 +1,19 @@
 #pragma once
 #include <stdbool.h>
 
-bool str_suffix(char *haystack, char *needle);
+bool str_suffix(char *haystack, char *needle); // function to check if a string ends with a suffix
 
-char *str_repeat(char *s, int n);
+char *str_repeat(char *s, int n); // function to repeat a string n times
 
-bool str_is_palindrome(char *s);
+bool str_is_palindrome(char *s); // function to check if a string is a palindrome
 
-float dot_product(float *vec_a, float *vec_b, int length);
+float dot_product(float *vec_a, float *vec_b, int length); // function to calculate the dot product of two vectors
 
-int reduce(int *nums, int length, int (*f)(int, int), int initial);
+int reduce(int *nums, int length, int (*f)(int, int), int initial); // function to reduce an array of integers
 
-int maximum_with_reduce(int *nums, int length);
+int maximum_with_reduce(int *nums, int length); // function to find the maximum value in an array of integers
 
-int sum_positive_with_reduce(int *nums, int length);
+int sum_positive_with_reduce(int *nums, int length); // function to find the sum of positive values in an array of integers
 
 // Idea for the UNUSED macro from Stack Overflow user mtvec. Thanks!
 // https://stackoverflow.com/a/3599170
