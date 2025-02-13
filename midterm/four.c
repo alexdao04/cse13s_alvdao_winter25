2b63a891845d08f707fee7827caf96a2ed07f073
alexdao <alvdao@ucsc.edu>
2025-02-13 15:04:43 -0800

    latest

--- midterm/four.c
+++ midterm/four.c
@@ -1,6 +1,9 @@
 #include "midterm.h"
-
 #include <stdio.h>
+
+// Function declarations
+int compare_customers(ShoeCustomer a, ShoeCustomer b);
+void swap_customers(ShoeCustomer *a, ShoeCustomer *b);
 
 // PROBLEM 3
 void bubble_sort_customers(ShoeCustomer *customers, size_t n_customers) {
@@ -11,7 +14,7 @@ void bubble_sort_customers(ShoeCustomer *customers, size_t n_customers) {
   // You can use the compare_customers function to compare two customers.
   // You can use the get_n_purchases function to get the number of purchases of a customer.
   // You can use the get_name function to get the name of a customer.
-  
+
   for(size_t i = 0; i < n_customers; i++) {
   // increments through number of customers
 
--- midterm/two.c
+++ midterm/two.c
@@ -1,59 +1,69 @@
 #include "midterm.h"
-
 #include <stdio.h>
 
 ll_float *delete_range(ll_float *list, float start, float end) {
-  // list is the head of the linked list
-  // start is the beginning of the range to delete
-  // end is the end of the range to delete
-  // we want to return the resulting linked list
+    // list is a pointer to the head of a linked list of floats
+    // start is the start of the range to delete
+    // end is the end of the range to delete
+    
+    // This function deletes all nodes in the linked list
+    // with values between start and end (inclusive)
+    // the function should return the head of the resulting linked list. 
+    // if the head of the linked list is deleted,
+    // the function should return the new head of the linked list.
 
-  ll_float *previous = NULL;
-  // previous is the previous value in the linked list
-  // we set it to NULL because we're not using it yet (initializing)
+    ll_float *previous = NULL;
+    // previous node is NULL to start with
 
-  ll_float *current_value = list;
-  // current_value is the current value in the linked list
-  // we set it to the head of the linked list
-  // which returns as our final resulting linked list
- 
-  while(current_value != NULL) {
-    // while the list is not empty
+    ll_float *current_value = list;
+    // current value is the head of the linked list
+    // we're going to iterate through the linked list
+    // and eventually return its final value
 
-    if(current_value-> value >= start && current_value -> value <= end) {
-      // if the current value is within the range
+    while (current_value != NULL) {
+    // while the current value is not NULL
+    // we're going to iterate through the linked list
+    // and delete the nodes that are within the range
 
-      if(previous == NULL) {
-        // if the previous value is NULL
+        if (current_value -> value >= start && current_value -> value <= end) {
+        // if the current value is within the range
+        // we're going to delete it
 
-        list = current_value -> next;
-        // set the list to the next value
+            if (previous == NULL) {
+            // if the previous node is NULL
 
-      } else {
-        // if the previous value is not NULL
+                list = current_value -> next;
+                // the new head of the linked list 
+                // is the next node after the current value
 
-        previous -> next = current_value -> next;
-        // set the next value of the previous value to the next value of the current value
+            } else { 
+            // if the previous node is not NULL
+                previous->next = current_value -> next;
+                // the previous node's next value
 
-      }
+            }
 
-      ll_float *temp_value = current_value;
-      // temp_value is the current value
+            ll_float *temp_value = current_value;
+            // temporary value is the current value
 
-      current_value = current_value -> next;
-      // set the current value to the next value
+            current_value = current_value -> next;
+            // the current value is the next value
 
-      free(temp_value);
-      // free the memory of the current value
+            free(temp_value);
+            // this frees the temporary value 
+            // (which is our current value) from memory
 
-    } else {
-      // if the current value is not within the range
+        } else {
+        // if the current value is not within the range
 
-      previous = current_value;
-      // set the previous value to the current value
+            previous = current_value;
+            // previous value is now the current value
 
+            current_value = current_value -> next;
+            // current value is now the next value
+        }
     }
 
-    return list;
+    return list; 
+    // return our resulting linked list
   }
-}
