07f0011cd5844d2119cabd7f37b1c5995f06b580
alexdao <alvdao@ucsc.edu>
2025-03-19 11:40:46 -0700

    final

--- final/two.c
+++ final/two.c
@@ -8,22 +8,48 @@ char *remove_characters(char *text, char *toremove) {
   // we need to remove characters from a string
   // we can do this by reading the string
   // if a matching character is found, we can remove it
-  char *result = text;
-  char *temp = text;
 
-  while (*text) {
-    char *toremove_temp = toremove;
-    while (*toremove_temp) {
-      if (*text == *toremove_temp) {
-        break;
-      }
-      toremove_temp++;
+    char *updated_text = malloc(strlen(text) + 1);
+      // start by allocating memory
+
+    if (!updated_text) {
+        return NULL; 
+        // just in case our string didnt get updated
     }
-    if (!*toremove_temp) {
-      *temp = *text;
-      temp++;
-    }
-    text++;
-  }
-return result;
+
+    char *updated_text_ptr = updated_text;
+    // stores the updated string
+    char *temp = text;
+    // stores the original string
+
+    while (*temp) {
+      // while loop to read the string
+        char *toremove_temp = toremove;
+          // initialize check to toremove
+
+        while (*toremove_temp) {
+          // check if the character is in the string
+            if (*temp == *toremove_temp) {
+              // if it is
+                break;
+                  // break out of the loop
+            }
+
+            toremove_temp++;
+              // increment check (because we're checking the next character)
+        }
+
+        if (!*toremove_temp) {
+          // if the pointer to check is empty
+            *updated_text_ptr++ = *temp;
+              // add the character to the new string
+        }
+
+        temp++;
+          // increment temp (because we're moving to the next character)
+      }          
+
+    *updated_text_ptr = '\0';
+    return updated_text;
+    // return the final result
 }
